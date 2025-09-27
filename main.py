f72f655e563331760dce214983584cf114ae93e6
Sara <122972681+Sarajona@users.noreply.github.com>
2025-09-27 17:05:00 +0200

    Finished highscore funktion

--- main.py
+++ main.py
@@ -2,16 +2,16 @@
 import random
 
 class Players:
-    def __init__(self):
+    def __init__(self, name):
         self.scores = []
         self.total_score = 0
         self.wins = 0
         self.highscore = 0
+        self.name = name
 
     def roll(self):
         score = random.randint(1,6)
         self.scores.append(score)
-        self.calculate_highscore
         print(f"The dice rolled: {score}")
 
     def calculate_total(self):
@@ -21,6 +21,7 @@ class Players:
     def calculate_highscore(self):
         if self.wins > self.highscore:
             self.highscore = self.wins
+            print(f"New {self.name} highscore: {self.highscore}")
 
     def reset(self):
         self.total_score = 0
@@ -28,35 +29,35 @@ class Players:
 
 class Game:
     def __init__(self):
-        self.player = Players()
-        self.dealer = Players()
-        self.highscores = self.read_file("Highscores.txt")
+        self.player = Players("player")
+        self.dealer = Players("dealer")
+        self.read_file("highscores.txt")
     
     def read_file(self, name):
-        print("Let me just read your previous wins and highscore...")
+        print("Loading previous highscores")
         try:
             with open(name) as file:
-                return list(map(lambda row: row.strip(), file))
-        except:
-            print("No previous wins or highscores found, starting with clean slate. Interesting.")
-            self.high_score = 0
-            self.player.wins = 0
-            self.dealer.wins = 0
+                lines = list(map(lambda row: row.strip(), file))
+                self.player.highscore = int(lines[0])
+                self.dealer.highscore = int(lines[1])
+                print(f"Your previous highscore i {self.player.highscore} and my previous highscore is {self.dealer.highscore}")
+        except FileNotFoundError:
+            self.player.highscore = 0
+            self.dealer.highscore = 0
+            print(f"No previous highscore recorded, starting with clean slate\nPlayer: {self.player.wins}\nDealer: {self.dealer.wins}")
             return []
     
-    def save_file(self, name, highscores):
-        print("Saving scores to file...")
+    def save_file(self, name):
+        print("Updating highscores file...")
         try:
             with open(name, "w") as file:
-                for score in highscores:
-                    file.write(f"{self.player.wins}\n")
-                    file.write(f"{self.dealer.wins}\n")
-                    file.write(f"{self.player.highscore}\n")
-                    file.write(f"{self.dealer.highscore}\n")
+                file.write(f"{self.player.highscore}\n")
+                file.write(f"{self.dealer.highscore}\n")
+
         except OSError:
-            print("Scores could not be saved for some reason")
+            print("File could not be updated for some reason")
         else:
-            print("The scores are now saved on file")
+            print("Highscores file has been updated")
 
     def play_a_round(self):
         
@@ -133,11 +134,10 @@ while play_game:
     if answer.lower() == "yes":
         print(f"NICE!! So you've won {game.player.wins} times and I've won {game.dealer.wins}")
     elif answer.lower() == "no":
-        print(f"Okay, then we'll leave it at:\nplayer:{game.player.wins}\ndealer:{game.dealer.wins}")
+        print(f"Okay, then we'll leave it at...\nplayer: {game.player.wins}\ndealer: {game.dealer.wins}. Coward")
         game.player.calculate_highscore()
         game.dealer.calculate_highscore()
-        game.save_file("highscores.txt", game.highscores)
-        print("Come back and play any time... Coward")
+        game.save_file("highscores.txt")
         play_game = False
         break
 
